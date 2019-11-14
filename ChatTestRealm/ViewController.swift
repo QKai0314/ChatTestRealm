ea11ecc648121643f16d5eac54a19edd248ef2b9
FRCMentor <kai0314@hotmail.com>
2019-11-14 09:13:29 +0800

    Initial Commit

---
+++ ChatTestRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatTestRealm
+//
+//  Created by Kai Qiao on 11/14/19.
+//  Copyright © 2019 iKai0314. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
